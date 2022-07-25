a01403473b7d81e6b6a645420e77a14610db0e17
Noor Awied <nooorawied@gmail.com>
2022-07-25 16:29:26 +0300

    initial project structure setup

--- src/app.js
+++ src/app.js
@@ -1,16 +1,53 @@
 const express = require('express');
 require('dotenv').config();
 
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
 const bodyParser = require('body-parser');
 const connectToMongo = require('./db/connection');
 
 const app = express();
-const port = process.env.NODE_LOCAL_PORT;
+const port = process.env.NODE_LOCAL_PORT || 3000;
 
 app.use(bodyParser.json());
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Swagger definition
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: "Let's Share API with Swagger",
+    version: '0.1.0',
+    description: "Let's Share API documentation with Swagger",
+    license: {
+      name: 'MIT',
+      url: 'https://spdx.org/licenses/MIT.html',
+    },
+  },
+  servers: [
+    {
+      url: 'http://localhost:3000',
+    },
+  ],
+  host: 'localhost:3000', // the host or url of the app
+};
+// options for the swagger docs
+const options = {
+  definition: swaggerDefinition,
+  apis: ['./docs/**/*.yaml'],
+};
+// initialize swagger-jsdoc
+const swaggerSpec = swaggerJsdoc(options);
+
+// use swagger-Ui-express for app documentation endpoint
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.get('/', (req, res) => {
+  res.send("Hello Let's Share!");
+});
 
 if (process.env.NODE_ENV !== 'test') {
   app.listen(port, () => {
