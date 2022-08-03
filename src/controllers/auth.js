aaafaebcf373e508879942b706812ec54e16fabd
niloaydin <nilay.aydn97@gmail.com>
2022-08-03 20:33:46 +0300

    Delete the unused code

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -32,21 +32,6 @@ const loginUsers = async (req, res) => {
   }
 };
 
-// eslint-disable-next-line consistent-return
-// const getProfile = async (req, res) => {
-//   try {
-//     if (req.user) {
-//       const currentUser = await UserModel.findById(req.user.userId);
-//       return res
-//         .setHeader('Content-Type', 'application/json')
-//         .status(200)
-//         .json(currentUser);
-//     }
-//   } catch (error) {
-//     return res.status(403).json({ message: error.message });
-//   }
-// };
-
 module.exports = {
   loginUsers,
 };
