9a8de88ea1d27be6964355438f7afb57f153b270
khaldar <adnan-5-@outlook.com>
2022-08-06 22:49:56 +0300

    Add different test case && fix requested revision

--- src/__tests__/user.spec.js
+++ src/__tests__/user.spec.js
@@ -16,9 +16,26 @@ afterAll(async () => {
 });
 
 describe('GET /api/user/:id', () => {
+  const validUserId = '62eb038c20b4f5b0bd8c7676';
   const invalidUserId = '62ee5c280262c62f56124dfd';
 
-  it('should return a found user', (done) => {
+  it('should return a single user', (done) => {
+    supertest(app)
+      .get(`/api/user/${validUserId}`)
+
+      .expect('Content-Type', /json/)
+      .expect(401, (err, res) => {
+        if (err) return done(err);
+        expect(res.status).toBe(401);
+        expect(res.body.error).toBe(true);
+        expect(res.body.message).toBe(
+          'Invalid Token: No authorization token was found'
+        );
+        return done();
+      });
+  });
+
+  it('should return "User not found"', (done) => {
     supertest(app)
       .get(`/api/user/${invalidUserId}`)
 
