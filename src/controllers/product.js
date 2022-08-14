b18b9051514526fa257d4c369f50974c22633d36
okurezgi <okurezgi@windowslive.com>
2022-08-14 19:45:26 +0300

    Added controller and documentation for updating product

---
+++ src/controllers/product.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-underscore-dangle */
+const _ = require('lodash');
+
+const ProductModel = require('../models/product');
+
+const updateProduct = async (req, res) => {
+  try {
+    const product = await ProductModel.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({
+        message: 'Product not found',
+      });
+    }
+
+    if (product.publisher.toString() !== req.user.userId.toString()) {
+      return res.status(403).json({
+        message: 'You are not authorized to update this product',
+      });
+    }
+
+    if (product.isTransactionCompleted) {
+      return res.status(400).json({
+        message:
+          'Transaction is already completed, you cannot update this product',
+      });
+    }
+
+    const validKeys = [
+      'title',
+      'description',
+      'photos',
+      'category',
+      'location',
+      'productCondition',
+      'shippingOptions',
+      'postType',
+    ];
+
+    const updatedProduct = _.pick(req.body, validKeys);
+    const updatedValidKeys = Object.keys(updatedProduct);
+    const requestedKeys = Object.keys(req.body);
+
+    if (updatedValidKeys.toString() !== requestedKeys.toString()) {
+      return res.status(422).json({
+        message:
+          'You entered an invalid key in the body, please check product for valid keys',
+      });
+    }
+
+    for (let i = 0; i < updatedValidKeys.length; i += 1) {
+      const key = updatedValidKeys[i];
+      console.log(key);
+      product[key] = updatedProduct[key];
+    }
+    product.save();
+    return res.status(200).json(product);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  updateProduct,
+};
--- src/routes/product.js
+++ src/routes/product.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
+const productController = require('../controllers/product');
+
+router.patch('/:id', productController.updateProduct);
+
 module.exports = router;
