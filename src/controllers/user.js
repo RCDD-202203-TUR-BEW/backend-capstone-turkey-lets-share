44d8a06cc85a3450ac45414340d31f3423d2e60c
Noor Awied <nooorawied@gmail.com>
2022-08-15 21:34:24 +0300

    made requested changes

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -36,16 +36,11 @@ const getSingleUser = async (req, res) => {
 
 const deleteProfile = async (req, res) => {
   try {
-    const foundUser = await UserModel.findById(req.params.id);
-    if (foundUser) {
-      if (req.user.userId === foundUser.id) {
-        await UserModel.findByIdAndDelete(req.params.id);
-        return res.status(200).json({ message: 'User deleted' });
-      }
-
-      return res.status(401).json({ message: 'Unauthorized' });
+    if (req.user) {
+      await UserModel.findByIdAndDelete(req.user.userId);
+      await res.clearCookie('token');
+      return res.status(200).json({ message: 'User deleted' });
     }
-
     return res.status(404).json({ message: 'User not found' });
   } catch (error) {
     return res.status(500).json({ message: error.message });
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const userController = require('../controllers/user');
+const authorize = require('../middleware/guard');
 
 const router = express.Router();
 
 router.get('/profile', userController.getProfile);
 router.get('/:id', userController.getSingleUser);
-router.delete('/:id', userController.deleteProfile);
+router.delete('/delete', authorize, userController.deleteProfile);
 module.exports = router;
