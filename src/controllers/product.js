0587ae033bedde97a707551cfd94b938f6da410b
khaldar <adnan-5-@outlook.com>
2022-08-29 17:02:05 +0300

    Send email when request is approved Fixes #78

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -5,7 +5,10 @@ const objectId = require('mongoose').Types.ObjectId;
 const constants = require('../lib/constants');
 const UserModel = require('../models/user');
 const ProductModel = require('../models/product');
-const { sendProductRequestEmail } = require('../services/mail');
+const {
+  sendProductRequestEmail,
+  sendProductApprovalEmail,
+} = require('../services/mail');
 
 const addNewProduct = async (req, res) => {
   try {
@@ -312,6 +315,13 @@ const approveRequest = async (req, res) => {
         message: 'Product not found',
       });
     }
+
+    if (String(product.publisher) !== req.user.userId) {
+      return res.status(401).json({
+        message: 'You are not authorized to perform this action',
+      });
+    }
+
     const requesterInOrders = product.orderRequests.includes(
       req.params.requesterId
     );
@@ -323,10 +333,38 @@ const approveRequest = async (req, res) => {
     }
     if (product.postType === 'Donate') {
       if (!product.isTransactionCompleted) {
+        const owner = await UserModel.findById(product.publisher);
+
         requester.received.push(product._id);
         requester.requested.remove(product._id);
         product.beneficiary = requester._id;
         product.isTransactionCompleted = true;
+
+        if (
+          !constants.PHONE_NUMBER_REGEX.test(owner.phoneNumber) ||
+          !owner.phoneNumber
+        ) {
+          await sendProductApprovalEmail(
+            requester.username,
+            requester.email,
+            product.title,
+            product._id,
+            owner.username,
+            owner.email,
+            owner._id
+          );
+        } else {
+          await sendProductApprovalEmail(
+            requester.username,
+            requester.email,
+            product.title,
+            product._id,
+            owner.username,
+            owner.email,
+            owner._id,
+            owner.phoneNumber
+          );
+        }
 
         requester.save();
         product.save();
--- src/services/mail.js
+++ src/services/mail.js
@@ -112,4 +112,70 @@ const sendProductRequestEmail = async (
   });
 };
 
-module.exports = { sendWelcomeEmail, sendProductRequestEmail };
+const sendProductApprovalEmail = async (
+  requesterUsername,
+  requesterEmail,
+  productTitle,
+  productId,
+  ownerUsername,
+  ownerEmail,
+  ownerId,
+  ownerPhoneNumber
+) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    secure: true,
+    auth: {
+      type: 'OAuth2',
+      user: process.env.GMAIL,
+      pass: process.env.GMAIL_PASSWORD,
+      clientId: process.env.GMAIL_CLIENT_ID,
+      clientSecret: process.env.GMAIL_CLIENT_SECRET,
+      refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+    },
+  });
+
+  let output;
+  if (ownerPhoneNumber) {
+    output = `
+      <p>Hello ${requesterUsername},<br>
+      <a href="https://lets-share-capstone.herokuapp.com/api/user/${ownerId}">${ownerUsername}</a> has accepted your request for <a href="https://lets-share-capstone.herokuapp.com/api/product/${productId}">${productTitle}</a>.<br>
+      You can contact ${ownerUsername} on ${ownerEmail} or ${ownerPhoneNumber}.</p>
+      <p>Kind regards,<br>
+      Let's Share team</p>
+      <br></br>
+      <p style="color:#a0a0a0; font-size:10px; text-align:center;">Let's Share, 2022. All rights reserved.</p>
+    `;
+  } else {
+    output = `
+      <p>Hello ${requesterUsername},<br>
+      <a href="https://lets-share-capstone.herokuapp.com/api/user/${ownerId}">${ownerUsername}</a> has accepted your request for <a href="https://lets-share-capstone.herokuapp.com/api/product/${productId}">${productTitle}</a>.<br>
+      You can contact ${ownerUsername} on ${ownerEmail}.</p>
+      <p>Kind regards,<br>
+      Let's Share team</p>
+      <br></br>
+      <p style="color:#a0a0a0; font-size:10px; text-align:center;">Let's Share, 2022. All rights reserved.</p>
+    `;
+  }
+
+  const mailOptions = {
+    from: `"Let's Share" <${process.env.GMAIL}>`,
+    to: `"${requesterUsername}" <${requesterEmail}>`,
+    subject: 'Your request has been accepted',
+    text: 'Your request has been accepted.',
+    html: output,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);
+  });
+};
+
+module.exports = {
+  sendWelcomeEmail,
+  sendProductRequestEmail,
+  sendProductApprovalEmail,
+};
